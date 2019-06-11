9f61d989a775ed0e630ee14672947a19d3bdbf54
Richard Basson <bassonrichard@gmail.com>
2019-06-11 17:35:06 +0200

    Landing Page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,65 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: "#ffffff",
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
+const styles = theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
+});
 
-export default function LandingPage() {
+class LandingPage extends Component {
 
-  const classes = useStyles();
-
-  return (
-    <div className="LandingPage">
-      <Typography variant="h1" align="center" color="primary" gutterBottom={true}>
-        Landing Page
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="LandingPage">
+        <Typography variant="h1" align="center" color="primary" gutterBottom={true}>
+          Landing Page
       </Typography>
 
-      <Paper>
-        <Grid container justify="center" alignItems="center" spacing={2}>
+        <Paper>
+          <Grid container justify="center" alignItems="center" spacing={2}>
 
-          <Grid item md={2}>
-          <Button  variant="outlined" color="primary" className={classes.submit}>
-            Login 
+            <Grid item md={2}>
+              <Button variant="outlined" color="primary" className={classes.submit}>
+                Login
           </Button>
+            </Grid>
+
+            <Grid item md={2}>
+              <Button variant="outlined" color="primary" className={classes.submit}>
+                Register
+          </Button>
+            </Grid>
+
           </Grid>
 
-          <Grid item md={2}>
-          <Button  variant="outlined" color="primary" className={classes.submit}>
-            Login 
-          </Button>
-          </Grid>
-
-        </Grid>
-
-      </Paper>
+        </Paper>
 
 
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
+export default withStyles(styles, { withTheme: true })(LandingPage);
